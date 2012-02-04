ceda446174abfb932586c5a5e32c20b145246717
Jason Begs <jbegleiter@pinpoynthealth.com>
2012-02-04 17:28:24 -0500

    First Commit Trial

---
+++ v0.1/lib.js
@@ -0,0 +1,31 @@
+function expression(polyval){
+
+	this.express = function(){
+		var i=0; var st = ""; var pow = 0;
+		for(i=0; i< polyval.length; i++){
+			if(polyval[i]!= 0){
+				sign = ""; val = polyval[i];
+				if(val>0 && st != "" ){ sign = " + "; }
+				else if(val<0 && st != "") { 
+					sign = " - "; 
+					val = val.toString().replace("-","");
+				}
+				if(polyval.length - i - 1!=0){
+					pow = "x<sup>" + (polyval.length - i - 1) + "</sup>";
+				} 
+				else { pow = ""}
+			//create string 
+				st = st + sign + val+ pow;
+			}
+		}
+		document.write(st);
+	}
+
+}
+function create_polyval(x0,x1,x2){
+	var poly = new Array();
+	poly[0] = x0;
+	poly[1] = x1;
+	poly[2] = x2;
+	return poly;
+}
