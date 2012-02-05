009599010237e36162bc7f53a319e10258cc0d83
Jason Begs <jbegleiter@pinpoynthealth.com>
2012-02-05 16:33:11 -0500

    Rand 2

--- v0.1/lib.js
+++ v0.1/lib.js
@@ -1,7 +1,7 @@
 function expression(){
 
 	this.express = function(html_replace,polyval){
-		var i=0; var st = ""; var pow = 0;
+		var st = ""; var pow = 0;
 		for(i=0; i< polyval.length; i++){
 			if(polyval[i]!= 0){
 				sign = ""; val = polyval[i];
@@ -20,19 +20,30 @@ function expression(){
 		}
 		document.getElementById(html_replace).innerHTML = st;
 	}	
-	this.generate = function(dim, html_replace){
+	this.generate = function(dim){
 		a = new Array();
 		for(i=0;i<dim;i++){
 			a[i] = Math.floor(Math.random()*11);
 		}
-		//document.getElementById(html_replace).innerHTML = a[0];
 		return a;
+	}
+	//this.opp = Math.floor(Math.random()*40);
+	this.solution = function(coef,opp,html_replace){
+		//used for quadratic for now
+		if(coef.length == 3){
+			sol = new Array();
+			var a = coef[0]; var b = coef[1]; var c = coef[2]; 
+			sol[0] = (b*-1)+Math.sqrt((Math.pow(b,2) - (4*a*c))/(2*a));
+			//document.getElementById(html_replace).innerHTML = sol[0];
+			//document.write(sol[0]);
+		}
+		return sol;
 	}
 }
 function gen(){
-//p = new Array(3,-1,3);
 ex = new expression();
-arr = ex.generate(4,"response");
+arr = ex.generate(4);
 ex.express("formula", arr);
-
+//ex.solution(arr.slice(0,2),arr.slice(-1),"response");
+document.getElementById("response").innerHTML = arr.slice(0,3);
 }
